80ed3c5c4b5630abc3f638bccc420a0c51465872
Isu World <isuworld22@gmail.com>
2025-08-26 23:06:24 +0530

    Add simple dice roller with roll again functionality
    - Implements basic 6-sided dice roll using random.randint()
    - Added loop to allow continuous rolling until user chooses to stop

---
+++ Beginner/Dice Roller.py
@@ -0,0 +1,9 @@
+import random
+
+def roll_dice():
+    return random.randint(1, 6)
+
+while True:
+    print(f"You rolled: {roll_dice()}")
+    if input("Roll again? (y/n): ").lower() != 'y':
+        break
